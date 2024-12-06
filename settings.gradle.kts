3945338d96b631d6d8274754ca6faa94bf2b1bf6
Xing Zhou <xing@xingzhou.me>
2024-12-06 14:53:21 -0500

    chore: correctly reformat in precommit hook

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,14 @@ rootProject.name = "workout"
 plugins { id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.15" }
 
 gitHooks {
-  preCommit { tasks("spotlessApply") }
+  preCommit {
+      from {
+          """
+              ./gradlew spotlessApply
+              git add --update
+          """.trimIndent()
+      }
+  }
   commitMsg { conventionalCommits { defaultTypes() } }
   createHooks()
 }
