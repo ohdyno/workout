97b08d63d4a355c1ff8b36120afadd840397097a
Xing Zhou <xing@xingzhou.me>
2024-12-08 10:49:33 -0500

    ci: disable installing git hooks in dokku environment

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,17 +2,19 @@ rootProject.name = "workout"
 
 plugins { id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.15" }
 
-gitHooks {
-  preCommit {
-    from {
-      """
+if (System.getenv("DOKKU_ROOT") == null) {
+  gitHooks {
+    preCommit {
+      from {
+        """
               ./gradlew spotlessApply
               git add --update
           """
-          .trimIndent()
+            .trimIndent()
+      }
     }
-  }
 
-  commitMsg { conventionalCommits { defaultTypes() } }
-  createHooks()
+    commitMsg { conventionalCommits { defaultTypes() } }
+    createHooks()
+  }
 }
