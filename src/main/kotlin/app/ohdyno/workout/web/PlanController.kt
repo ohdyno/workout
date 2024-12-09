e609f3424b5d0cb46e801961912d52f7aff5442b
Xing Zhou <xing@xingzhou.me>
2024-12-09 14:13:32 -0500

    feat: add static bb upper-lower plan

---
+++ src/main/kotlin/app/ohdyno/workout/web/PlanController.kt
@@ -0,0 +1,14 @@
+package app.ohdyno.workout.web
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/plans")
+class PlanController {
+  @GetMapping("/bb-upper-lower")
+  fun bbUpperLower(): String {
+    return "pages/plans/bb-upper-lower"
+  }
+}
